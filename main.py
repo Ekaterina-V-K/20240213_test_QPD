4ea87a70126fff062f7a51e0418be51de1a40350
Ekaterina_K <kuznetsova.ekaterina.v@gmail.com>
2024-02-15 23:09:10 +0300

    20240215

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
-print("Введите целое число или 0")
-n = int(input())
+def InputNum():
+    while True:
+        number = input("Введите целое число или 0:")
+        try:
+            number = int(number)
+            break
+        except ValueError:
+            print("Ошибка: введите корректное число")
+    return number
+
+
+n = InputNum()
 max_s = 0
 max_m = 0
 while n != 0:
     m = n
     s = 0
+    n=abs(n) #сумму цифр отрицательного числа считаем без учета знака "-"
     while n>0:
         s = s+(n%10)
         n = n//10
     if s > max_s:
         max_s = s
         max_m = m
-    print("Введите целое число или 0")
-    n = int(input())
+    n = InputNum()
 print('Число',max_m,'имеет максимальную сумму цифр:', max_s)
+
+
+
